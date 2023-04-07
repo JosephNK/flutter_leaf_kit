d864036c2554c6b2f74ce396d8e50724511dddb0
JosephNK <nkw0608@gmail.com>
2023-04-08 01:53:23 +0900

    feat: working..

--- packages/leaf_component/lib/src/component/dialog/component/lf_alert_dialog.dart
+++ packages/leaf_component/lib/src/component/dialog/component/lf_alert_dialog.dart
@@ -9,15 +9,26 @@ class LFAlertDialog {
     BuildContext context, {
     String? title,
     required String message,
-    VoidCallback? onOk,
+    String? okText,
+    TextStyle? titleStyle,
+    TextStyle? messageStyle,
+    TextStyle? okTextStyle,
+    Color? okTextBackgroundColor,
+    VoidCallback? onOK,
   }) async {
-    final okText = LFComponentConfigure.shared.alert?.okText ?? 'OK';
-    await _LFAlertDialog.show(
+    final okTextStr =
+        okText ?? LFComponentConfigure.shared.alert?.okText ?? 'OK';
+
+    await _LFAlertDialog().show(
       context,
       title: title,
       message: message,
-      onOK: onOk,
-      okText: okText,
+      titleStyle: titleStyle,
+      messageStyle: messageStyle,
+      onOK: onOK,
+      okTextStyle: okTextStyle,
+      okTextBackgroundColor: okTextBackgroundColor,
+      okText: okTextStr,
     );
   }
 
@@ -25,20 +36,35 @@ class LFAlertDialog {
     BuildContext context, {
     String? title,
     required String message,
-    Function? onCancel,
-    Function? onOK,
+    TextStyle? titleStyle,
+    TextStyle? messageStyle,
+    String? okText,
+    TextStyle? okTextStyle,
+    Color? okTextBackgroundColor,
+    TextStyle? cancelTextStyle,
+    Color? cancelTextBackgroundColor,
+    VoidCallback? onCancel,
+    VoidCallback? onOK,
   }) async {
-    final cancelText = LFComponentConfigure.shared.alert?.cancelText ?? 'Close';
-    final okText = LFComponentConfigure.shared.alert?.okText ?? 'OK';
+    final cancelTextStr =
+        LFComponentConfigure.shared.alert?.cancelText ?? 'Close';
+    final okTextStr =
+        okText ?? LFComponentConfigure.shared.alert?.okText ?? 'OK';
 
-    await _LFAlertDialog.confirm(
+    await _LFAlertDialog().confirm(
       context,
       title: title,
       message: message,
+      titleStyle: titleStyle,
+      messageStyle: messageStyle,
       onCancel: onCancel,
       onOK: onOK,
-      cancelText: cancelText,
-      okText: okText,
+      okTextStyle: okTextStyle,
+      okTextBackgroundColor: okTextBackgroundColor,
+      okText: okTextStr,
+      cancelTextStyle: cancelTextStyle,
+      cancelTextBackgroundColor: cancelTextBackgroundColor,
+      cancelText: cancelTextStr,
     );
   }
 
@@ -48,14 +74,16 @@ class LFAlertDialog {
     VoidCallback? onTap,
   }) async {
     if (errorMessage == null) return;
-    final okText = LFComponentConfigure.shared.alert?.okText ?? 'OK';
+
+    final okTextStr = LFComponentConfigure.shared.alert?.okText ?? 'OK';
+
     String? errorTitle = 'Oops! Error :(';
-    _LFAlertDialog.show(
+    _LFAlertDialog().show(
       context,
       title: errorTitle,
       message: errorMessage,
       onOK: onTap,
-      okText: okText,
+      okText: okTextStr,
     );
   }
 
@@ -65,27 +93,34 @@ class LFAlertDialog {
     VoidCallback? onTap,
   }) async {
     if (exception == null) return;
-    final okText = LFComponentConfigure.shared.alert?.okText ?? 'Close';
+
+    final okTextStr = LFComponentConfigure.shared.alert?.okText ?? 'Close';
+
     String? errorTitle = 'Oops! Exception :(';
     String? errorMessage = exception.toString();
+
     if (isNotEmpty(errorMessage)) {
-      _LFAlertDialog.show(
+      _LFAlertDialog().show(
         context,
         title: errorTitle,
         message: errorMessage,
         onOK: onTap,
-        okText: okText,
+        okText: okTextStr,
       );
     }
   }
 }
 
 class _LFAlertDialog {
-  static Future<void> show(
+  Future<void> show(
     BuildContext context, {
     String? title,
     String? message,
-    required String okText,
+    TextStyle? titleStyle,
+    TextStyle? messageStyle,
+    String okText = 'OK',
+    TextStyle? okTextStyle,
+    Color? okTextBackgroundColor,
     VoidCallback? onOK,
   }) async {
     await showDialog(
@@ -94,43 +129,37 @@ class _LFAlertDialog {
         return AlertDialog(
           title: isEmpty(title)
               ? null
-              : LFText(
-                  title ?? '',
-                  style: const TextStyle(fontSize: 18),
-                  maxLines: 2,
-                ),
-          content: LFText(
-            message ?? '',
-            style: const TextStyle(fontSize: 16),
-            maxLines: 5,
-          ),
+              : _buildTitle(context, text: title, textStyle: titleStyle),
+          content:
+              _buildMessage(context, text: message, textStyle: messageStyle),
           actions: [
-            TextButton(
-              onPressed: () async {
-                Navigator.maybePop(context);
-                await Future.delayed(const Duration(milliseconds: 100));
-                onOK?.call();
-              },
-              child: LFText(
-                okText,
-                style: const TextStyle(fontSize: 18),
-              ),
-            ),
+            _buildOKButton(context,
+                autoPop: true,
+                text: okText,
+                textStyle: okTextStyle,
+                backgroundColor: okTextBackgroundColor,
+                onPressed: onOK),
           ],
         );
       },
     );
   }
 
-  static Future<void> confirm(
+  Future<void> confirm(
     BuildContext context, {
     String? title,
     String? message,
+    TextStyle? titleStyle,
+    TextStyle? messageStyle,
     bool autoPop = true,
-    required String okText,
-    required String cancelText,
-    Function? onCancel,
-    Function? onOK,
+    String okText = 'OK',
+    String cancelText = 'Cancel',
+    TextStyle? okTextStyle,
+    Color? okTextBackgroundColor,
+    TextStyle? cancelTextStyle,
+    Color? cancelTextBackgroundColor,
+    VoidCallback? onCancel,
+    VoidCallback? onOK,
   }) async {
     await showDialog(
       context: context,
@@ -138,46 +167,111 @@ class _LFAlertDialog {
         return AlertDialog(
           title: isEmpty(title)
               ? null
-              : LFText(
-                  title ?? '',
-                  style: const TextStyle(fontSize: 18),
-                  maxLines: 2,
-                ),
-          content: LFText(
-            message ?? '',
-            style: const TextStyle(fontSize: 16),
-            maxLines: 5,
-          ),
+              : _buildTitle(context, text: title, textStyle: titleStyle),
+          content:
+              _buildMessage(context, text: message, textStyle: messageStyle),
           actions: [
-            TextButton(
-              onPressed: () async {
-                if (autoPop) {
-                  Navigator.maybePop(context);
-                  await Future.delayed(const Duration(milliseconds: 100));
-                }
-                onCancel?.call();
-              },
-              child: LFText(
-                cancelText,
-                style: const TextStyle(fontSize: 18),
-              ),
-            ),
-            TextButton(
-              onPressed: () async {
-                if (autoPop) {
-                  Navigator.maybePop(context);
-                  await Future.delayed(const Duration(milliseconds: 100));
-                }
-                onOK?.call();
-              },
-              child: LFText(
-                okText,
-                style: const TextStyle(fontSize: 18),
-              ),
-            ),
+            _buildCancelButton(context,
+                autoPop: autoPop,
+                text: cancelText,
+                textStyle: cancelTextStyle,
+                backgroundColor: cancelTextBackgroundColor,
+                onPressed: onCancel),
+            _buildOKButton(context,
+                autoPop: autoPop,
+                text: okText,
+                textStyle: okTextStyle,
+                backgroundColor: okTextBackgroundColor,
+                onPressed: onOK),
           ],
         );
       },
+    );
+  }
+
+  Widget _buildTitle(
+    BuildContext context, {
+    String? text = 'Title',
+    TextStyle? textStyle,
+  }) {
+    return LFText(
+      text ?? '',
+      style: textStyle ?? const TextStyle(fontSize: 18),
+      maxLines: 2,
+    );
+  }
+
+  Widget _buildMessage(
+    BuildContext context, {
+    String? text = 'Message',
+    TextStyle? textStyle,
+  }) {
+    return LFText(
+      text ?? '',
+      style: textStyle ?? const TextStyle(fontSize: 16),
+      maxLines: 5,
+    );
+  }
+
+  Widget _buildCancelButton(
+    BuildContext context, {
+    bool autoPop = true,
+    String text = 'Cancel',
+    TextStyle? textStyle,
+    Color? backgroundColor,
+    VoidCallback? onPressed,
+  }) {
+    return GestureDetector(
+      onTap: () async {
+        if (autoPop) {
+          Navigator.maybePop(context);
+          await Future.delayed(const Duration(milliseconds: 100));
+        }
+        onPressed?.call();
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: backgroundColor ?? Colors.grey.withOpacity(0.5),
+        ),
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+        child: LFText(
+          text,
+          style: textStyle ?? const TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOKButton(
+    BuildContext context, {
+    bool autoPop = true,
+    String text = 'OK',
+    TextStyle? textStyle,
+    Color? backgroundColor,
+    VoidCallback? onPressed,
+  }) {
+    return GestureDetector(
+      onTap: () async {
+        if (autoPop) {
+          Navigator.maybePop(context);
+          await Future.delayed(const Duration(milliseconds: 100));
+        }
+        onPressed?.call();
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: backgroundColor ?? Colors.blueAccent,
+        ),
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+        child: LFText(
+          text,
+          style: textStyle ?? const TextStyle(fontSize: 18),
+        ),
+      ),
     );
   }
 }
