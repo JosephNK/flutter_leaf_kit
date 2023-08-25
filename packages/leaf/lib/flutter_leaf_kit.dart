7cef48050bccec0706e24d1f30081e7ecc86e047
JosephNK <nkw0608@gmail.com>
2023-08-26 01:47:29 +0900

    feat: Firebase 패키지 삭제. (패키지와 불가 :: 앱에서 처리 필요)

--- packages/leaf/lib/flutter_leaf_kit.dart
+++ packages/leaf/lib/flutter_leaf_kit.dart
@@ -3,6 +3,5 @@ library flutter_leaf_kit;
 export 'package:leaf_common/leaf_common.dart';
 export 'package:leaf_component/leaf_component.dart';
 export 'package:leaf_data/leaf_data.dart';
-export 'package:leaf_firebase/leaf_firebase.dart';
 export 'package:leaf_manager/leaf_manager.dart';
 export 'package:leaf_navigation/leaf_navigation.dart';
--- packages/leaf/lib/flutter_leaf_kit_firebase.dart
+++
@@ -1,3 +0,0 @@
-library flutter_leaf_kit_firebase;
-
-export 'package:leaf_firebase/leaf_firebase.dart';
