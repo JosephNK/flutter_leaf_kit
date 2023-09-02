1d941c0b31abe032219eddf45138319438ac3f44
JosephNK <nkw0608@gmail.com>
2023-09-02 20:04:07 +0900

    feat: bloc transformer 추가.

--- packages/leaf_data/lib/leaf_data.dart
+++ packages/leaf_data/lib/leaf_data.dart
@@ -4,6 +4,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:leaf_component/leaf_component.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 export 'package:bloc_concurrency/bloc_concurrency.dart';
 export 'package:equatable/equatable.dart';
@@ -15,5 +16,6 @@ part 'src/bloc/lf_bloc_observer.dart';
 part 'src/bloc/lf_bloc_screen_consumer.dart';
 part 'src/model/lf_data_model.dart';
 part 'src/model/lf_ui_model.dart';
+part 'src/transformer/lf_transformer.dart';
 part 'src/value/lf_error_value.dart';
 part 'src/value/lf_result_value.dart';
