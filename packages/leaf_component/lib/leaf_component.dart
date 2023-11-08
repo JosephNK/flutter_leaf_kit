31bbc8d6aec0c67e019bb641042922afd90e16fb
JosephNK <nkw0608@gmail.com>
2023-11-08 18:39:47 +0900

    feat: photo manager Hide LatLng.

--- packages/leaf_component/lib/leaf_component.dart
+++ packages/leaf_component/lib/leaf_component.dart
@@ -1,5 +1,6 @@
 library leaf_component;
 
+export 'package:photo_manager/photo_manager.dart' hide LatLng;
 export 'package:visibility_detector/visibility_detector.dart';
 
 export 'src/lf_component.dart';
