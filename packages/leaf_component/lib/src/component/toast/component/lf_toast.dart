8eded763f5808cf47ad555c0c7c766cb1b581529
JosephNK <nkw0608@gmail.com>
2024-05-11 13:55:41 +0900

    feat: update LFToast.

--- packages/leaf_component/lib/src/component/toast/component/lf_toast.dart
+++ packages/leaf_component/lib/src/component/toast/component/lf_toast.dart
@@ -1,17 +1,57 @@
 part of '../lf_toast.dart';
 
+enum LFToastType {
+  lengthShort,
+  lengthLong,
+}
+
+extension LFToastTypeExt on LFToastType {
+  Toast get value {
+    switch (this) {
+      case LFToastType.lengthShort:
+        return Toast.LENGTH_SHORT;
+      case LFToastType.lengthLong:
+        return Toast.LENGTH_LONG;
+    }
+  }
+}
+
+enum LFToastGravityType {
+  top,
+  center,
+  bottom,
+}
+
+extension LFToastGravityTypeExt on LFToastGravityType {
+  ToastGravity get value {
+    switch (this) {
+      case LFToastGravityType.top:
+        return ToastGravity.TOP;
+      case LFToastGravityType.center:
+        return ToastGravity.CENTER;
+      case LFToastGravityType.bottom:
+        return ToastGravity.BOTTOM;
+    }
+  }
+}
+
 class LFToast {
   static Future<bool?> show(
     BuildContext context, {
     required String message,
+    LFToastType toastType = LFToastType.lengthShort,
+    LFToastGravityType toastGravityType = LFToastGravityType.bottom,
+    Color? backgroundColor,
+    Color? textColor,
+    double? fontSize,
   }) {
     return Fluttertoast.showToast(
       msg: message,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.BOTTOM,
-      backgroundColor: Colors.black87,
-      textColor: Colors.white,
-      fontSize: 16.0,
+      toastLength: toastType.value,
+      gravity: toastGravityType.value,
+      backgroundColor: backgroundColor ?? Colors.black87,
+      textColor: textColor ?? Colors.white,
+      fontSize: fontSize ?? 16.0,
     );
   }
 }
