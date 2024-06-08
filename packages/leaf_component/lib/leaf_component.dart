62100c632d59833ddf5a10a42eb955243ef66c8b
JosephNK <nkw0608@gmail.com>
2024-06-08 19:29:12 +0900

    feat: Update LFRatingBar.

--- packages/leaf_component/lib/leaf_component.dart
+++ packages/leaf_component/lib/leaf_component.dart
@@ -2,6 +2,7 @@ library leaf_component;
 
 export 'package:flutter_cache_manager/flutter_cache_manager.dart'
     show CacheManager, Config, JsonCacheInfoRepository, HttpFileService;
+export 'package:flutter_rating_bar/flutter_rating_bar.dart' show RatingWidget;
 export 'package:photo_manager/photo_manager.dart' hide LatLng;
 export 'package:toastification/toastification.dart' show ToastificationItem;
 export 'package:visibility_detector/visibility_detector.dart';
--- packages/leaf_component/lib/src/component/ratingbar/component/lf_ratingbar.dart
+++ packages/leaf_component/lib/src/component/ratingbar/component/lf_ratingbar.dart
@@ -4,7 +4,9 @@ class LFRatingBar extends StatelessWidget {
   final int itemCount;
   final double initialRating;
   final double minRating;
-  final Widget? icon;
+  final Widget? iconWidget;
+  final RatingWidget? ratingWidget;
+  final EdgeInsets? itemPadding;
   final ValueChanged<double>? onRatingUpdate;
 
   const LFRatingBar({
@@ -12,25 +14,43 @@ class LFRatingBar extends StatelessWidget {
     required this.itemCount,
     this.initialRating = 0.0,
     this.minRating = 0.0,
-    this.icon,
+    this.iconWidget,
+    this.ratingWidget,
+    this.itemPadding,
     this.onRatingUpdate,
   });
 
   @override
   Widget build(BuildContext context) {
+    if (ratingWidget != null) {
+      return RatingBar(
+        initialRating: initialRating,
+        minRating: minRating,
+        direction: Axis.horizontal,
+        allowHalfRating: true,
+        itemCount: itemCount,
+        itemPadding: itemPadding ?? const EdgeInsets.symmetric(horizontal: 4.0),
+        ratingWidget: ratingWidget ??
+            RatingWidget(
+              full: const Icon(Icons.star, color: Colors.red),
+              half: const Icon(Icons.star_half, color: Colors.red),
+              empty: const Icon(Icons.star_border, color: Colors.red),
+            ),
+        onRatingUpdate: (rating) {
+          onRatingUpdate?.call(rating);
+        },
+      );
+    }
     return RatingBar.builder(
       initialRating: initialRating,
       minRating: minRating,
       direction: Axis.horizontal,
       allowHalfRating: true,
       itemCount: itemCount,
-      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-      itemBuilder: (context, _) =>
-          icon ??
-          const Icon(
-            Icons.star,
-            color: Colors.amber,
-          ),
+      itemPadding: itemPadding ?? const EdgeInsets.symmetric(horizontal: 4.0),
+      itemBuilder: (context, _) {
+        return iconWidget ?? const Icon(Icons.star, color: Colors.red);
+      },
       onRatingUpdate: (rating) {
         onRatingUpdate?.call(rating);
       },
