7b0bd81fcc92623feb12b436c40008e174da996b
JosephNK <nkw0608@gmail.com>
2024-06-08 19:45:48 +0900

    feat: Fixed LFRatingBar.

--- packages/leaf_component/lib/src/component/ratingbar/component/lf_ratingbar.dart
+++ packages/leaf_component/lib/src/component/ratingbar/component/lf_ratingbar.dart
@@ -2,55 +2,48 @@ part of '../ratingbar.dart';
 
 class LFRatingBar extends StatelessWidget {
   final int itemCount;
+  final RatingWidget? ratingWidget;
   final double initialRating;
   final double minRating;
-  final Widget? iconWidget;
-  final RatingWidget? ratingWidget;
-  final EdgeInsets? itemPadding;
+  final EdgeInsets itemPadding;
+  final double itemSize;
+  final bool tapOnlyMode;
+  final bool updateOnDrag;
   final ValueChanged<double>? onRatingUpdate;
 
   const LFRatingBar({
     super.key,
     required this.itemCount,
+    this.ratingWidget,
     this.initialRating = 0.0,
     this.minRating = 0.0,
-    this.iconWidget,
-    this.ratingWidget,
-    this.itemPadding,
+    this.itemPadding = EdgeInsets.zero,
+    this.itemSize = 40.0,
+    this.tapOnlyMode = false,
+    this.updateOnDrag = false,
     this.onRatingUpdate,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (ratingWidget != null) {
-      return RatingBar(
-        initialRating: initialRating,
-        minRating: minRating,
-        direction: Axis.horizontal,
-        allowHalfRating: true,
-        itemCount: itemCount,
-        itemPadding: itemPadding ?? const EdgeInsets.symmetric(horizontal: 4.0),
-        ratingWidget: ratingWidget ??
-            RatingWidget(
-              full: const Icon(Icons.star, color: Colors.red),
-              half: const Icon(Icons.star_half, color: Colors.red),
-              empty: const Icon(Icons.star_border, color: Colors.red),
-            ),
-        onRatingUpdate: (rating) {
-          onRatingUpdate?.call(rating);
-        },
-      );
-    }
-    return RatingBar.builder(
+    final ratingWidget = this.ratingWidget;
+
+    return RatingBar(
       initialRating: initialRating,
       minRating: minRating,
       direction: Axis.horizontal,
       allowHalfRating: true,
       itemCount: itemCount,
-      itemPadding: itemPadding ?? const EdgeInsets.symmetric(horizontal: 4.0),
-      itemBuilder: (context, _) {
-        return iconWidget ?? const Icon(Icons.star, color: Colors.red);
-      },
+      itemSize: itemSize,
+      itemPadding: itemPadding,
+      tapOnlyMode: tapOnlyMode,
+      updateOnDrag: updateOnDrag,
+      ratingWidget: ratingWidget ??
+          RatingWidget(
+            full: const Icon(Icons.star, color: Colors.red),
+            half: const Icon(Icons.star_half, color: Colors.red),
+            empty: const Icon(Icons.star_border, color: Colors.red),
+          ),
       onRatingUpdate: (rating) {
         onRatingUpdate?.call(rating);
       },
