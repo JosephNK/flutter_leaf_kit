6dd8d76baac8f9a1b7fbf12862bd081c3ae47e4f
JosephNK <nkw0608@gmail.com>
2024-07-30 11:04:51 +0900

    feat: HttpDioHelper 추가.

---
+++ packages/leaf_network/lib/src/http_dio_helper/http_dio_helper.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_leaf_store/leaf_store.dart';
+import 'package:http_parser/http_parser.dart';
+
+enum HttpDioMediaMimeType {
+  image,
+}
+
+extension HttpDioMediaMimeTypeExt on HttpDioMediaMimeType {
+  String get value {
+    switch (this) {
+      case HttpDioMediaMimeType.image:
+        return 'image';
+    }
+  }
+}
+
+class HttpDioHelper {
+  MultipartFile? convertFromFile(
+    LFMultipartFile file, {
+    List<String> allowExt = const ['.jpg', '.jpeg', '.png'],
+    HttpDioMediaMimeType mediaMimeType = HttpDioMediaMimeType.image,
+  }) {
+    final path = file.getPath();
+    final ext = file.getExtension();
+    if (path == null || ext == null) return null;
+    if (!allowExt.contains(ext)) return null;
+    final extension = ((ext == '.jpg') ? '.jpeg' : ext).replaceAll('.', '');
+    final fileName = file.getPayload<String>();
+    return MultipartFile.fromFileSync(
+      path,
+      filename: '$fileName.$extension',
+      contentType: MediaType(mediaMimeType.value, extension),
+    );
+  }
+}
