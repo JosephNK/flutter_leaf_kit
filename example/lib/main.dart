1bfdb16982dc91701fdc0fa95f054184642c1120
JosephNK <nkw0608@gmail.com>
2025-01-17 16:01:11 +0900

    feat: useMaterial3 : false 설정.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -92,7 +92,7 @@ class ExampleApp extends StatelessWidget {
             // navigatorObservers: [
             //   FirebaseAnalyticsObserver(analytics: analytics),
             // ],
-            theme: ThemeData(useMaterial3: true),
+            theme: ThemeData(useMaterial3: false),
             builder: (context, widget) {
               // ErrorWidget.builder = (errorDetails) {
               //   return BFRenderErrorPage(
