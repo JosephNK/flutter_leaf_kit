4aa06e73e504c42a5e63188d7071aabb83d9cea1
JosephNK <nkw0608@gmail.com>
2025-01-28 12:53:02 +0900

    chore: Update version 1.9.93-dev

--- packages/leaf_component/lib/leaf_component.dart
+++ packages/leaf_component/lib/leaf_component.dart
@@ -5,6 +5,7 @@ export 'package:flutter_cache_manager/flutter_cache_manager.dart'
 export 'package:flutter_rating_bar/flutter_rating_bar.dart' show RatingWidget;
 export 'package:photo_manager/photo_manager.dart' hide LatLng;
 export 'package:toastification/toastification.dart' show ToastificationItem;
+export 'package:vector_graphics/vector_graphics.dart' show AssetBytesLoader;
 export 'package:visibility_detector/visibility_detector.dart';
 
 export 'src/component.dart';
