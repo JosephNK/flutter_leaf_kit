1cb1acfeaf245b6b0df5765899085503ea43607d
JosephNK <nkw0608@gmail.com>
2025-06-05 19:37:04 +0900

    chore: Update version 2.3.2-dev

--- packages/leaf_common/lib/src/utils/type_converter.dart
+++ packages/leaf_common/lib/src/utils/type_converter.dart
@@ -37,6 +37,20 @@ class LFTypeConverter {
         }
       }
 
+      // num 타입 변환
+      if (T == num) {
+        if (value is num) return value as T;
+        if (value is String) {
+          // 정수로 파싱ㄴ 먼저 시도
+          if (value.contains('.')) {
+            return double.parse(value) as T;
+          } else {
+            return int.parse(value) as T;
+          }
+        }
+        if (value is bool) return (value ? 1 : 0) as T;
+      }
+
       // List 타입 변환
       if (T.toString().startsWith('List')) {
         if (value is List) return value as T;
